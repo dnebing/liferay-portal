aca6ac4f99bf780971551ebeae898d0c868cea09
dnebing <david.nebinger@liferay.com>
2017-05-30 18:55:43 -0400

    LPS-72751 Add action handler to spring mvc portlet template

--- modules/sdk/project-templates-spring-mvc-portlet/src/main/resources/archetype-resources/src/main/java/portlet/__className__PortletViewController.java
+++ modules/sdk/project-templates-spring-mvc-portlet/src/main/resources/archetype-resources/src/main/java/portlet/__className__PortletViewController.java
@@ -1,5 +1,7 @@
 package ${package}.portlet;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
@@ -17,5 +19,65 @@ public class ${className}PortletViewController {
 	@RenderMapping
 	public String view(RenderRequest request, RenderResponse response) {
 		return "view";
+	}
+
+	/**
+	 * Returns the view when the action key is set to <code>sayHello</code>.
+	 *
+	 * @param  request the render request
+	 * @param  response the render response
+	 * @return the view result
+	 */
+	@RenderMapping(params = "action=sayHello")
+	public String sayHello(RenderRequest request, RenderResponse response) {
+		return "say_hello";
+	}
+
+	/**
+	 * Handles the say hello action.
+	 * @param  request the action request
+	 * @param  response the action response
+	 */
+	@ActionMapping(params = "action=sayHello")
+	public void sayHello(ActionRequest request, ActionResponse response){
+
+		String name = ParamUtil.getString(request, "userName", "");
+
+		// fix the capitalization on the name
+		name = capitalizeFully(name);
+
+		// hide the success message.
+		hideDefaultSuccessMessage(request);
+
+		response.setRenderParameter("name", name);
+	}
+
+	/**
+	 * capitalizeFully: Capitalizes first letter of all words in given string.
+	 * @param str String to capitalize.
+	 * @return String The fully capitalized string.
+	 */
+	public String capitalizeFully(String str) {
+		if (str == null || str.length() == 0) {
+			return str;
+		}
+		int strLen = str.length();
+		str = str.toLowerCase();
+		StringBuffer buffer = new StringBuffer(strLen);
+		boolean capitalizeNext = true;
+		for (int i = 0; i < strLen; i++) {
+			char ch = str.charAt(i);
+
+			if (Character.isWhitespace(ch)) {
+				buffer.append(ch);
+				capitalizeNext = true;
+			} else if (capitalizeNext) {
+				buffer.append(Character.toTitleCase(ch));
+				capitalizeNext = false;
+			} else {
+				buffer.append(ch);
+			}
+		}
+		return buffer.toString();
 	}
 }
